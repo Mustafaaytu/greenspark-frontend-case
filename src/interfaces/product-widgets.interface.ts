48fd9ff9ebd7c8590c1245f44aee6434ff2f03a8
Mustafaaytu <mustafaaytu@gmail.com>
2024-03-03 13:03:06 +0300

    GRNSK-001 state structers created.

---
+++ src/interfaces/product-widgets.interface.ts
@@ -0,0 +1,9 @@
+export interface ProductWidget {
+  id: number;
+  type: string;
+  amount: number;
+  action: string;
+  active: boolean;
+  linked: boolean;
+  selectedColor: string;
+}
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,7 +1,16 @@
-import { Welcome } from '../components/Welcome/Welcome';
+import { fetchProductWidgets } from '@/store/actions/product-widgets.actions';
+import { useEffect } from 'react';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Welcome } from '../components/Welcome/Welcome';
+import { AppDispatch, useAppDispatch } from '../store/store';
 
 export function HomePage() {
+  const dispatch: AppDispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchProductWidgets());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Welcome />
